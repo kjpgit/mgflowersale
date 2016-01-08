6cd30aaa15da384becf436a4331741ccf8b6ee4c
kjpgit <kjpgit@users.noreply.github.com>
2016-01-08 13:21:14 -0600

    add

---
+++ images.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import subprocess
+
+import sale
+
+
+def err(s):
+    sys.stderr.write(s + "\n")
+    #sys.exit(1)
+
+
+def get_info(f):
+    p = subprocess.Popen(["mediainfo", f], stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    rc = p.wait()
+    assert rc == 0
+    ret = {}
+    lines = stdout.split("\n")
+    for line in lines:
+        if not line.strip():
+            continue
+        if ":" not in line:
+            continue
+        parts = line.split(":", 1)
+        key = parts[0].strip()
+        value = parts[1].strip()
+        if key in ("Format", "Width", "Height", "File size"):
+            ret[key] = value
+    ret["name"] = f
+    return ret
+
+
+sale.load_data()
+for item in sale.g_items:
+    f = item.image_file
+    if not os.path.exists(f):
+        err("not found: %s" % f)
+    else:
+        info = get_info(f)
+        print info["File size"], info["Format"], info["Height"], info['Width'], info['name']
+
