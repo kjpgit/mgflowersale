374f90c81fb966bb01728249f62c869365e159a9
kjpgit <kjpgit@users.noreply.github.com>
2016-01-08 13:30:07 -0600

    total size

--- images.py
+++ images.py
@@ -32,11 +32,16 @@ def get_info(f):
 
 
 sale.load_data()
+total_size = 0
 for item in sale.g_items:
     f = item.image_file
     if not os.path.exists(f):
         err("not found: %s" % f)
     else:
         info = get_info(f)
-        print info["File size"], info["Format"], info["Height"], info['Width'], info['name']
+        print info["File size"], info["Format"], \
+                info["Height"], info['Width'], info['name']
+        total_size += float(info["File size"].split()[0])
+
+sys.stderr.write("total file size: %s\n" % total_size)
 
